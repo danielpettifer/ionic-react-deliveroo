31eef4414996e1cf23dbdc9f6ad8c8e045eae1b8
Daniel Pettifer <danielpettifer@me.com>
2020-03-21 22:15:59 +0000

    added horizontal card component

---
+++ src/components/HorizontalCardList/HorizontalCardList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { IonSegment, IonSegmentButton, IonLabel } from '@ionic/react';
+import './HorizontalCardList.css';
+
+class HorizontalCardList extends React.Component {
+
+  render() {
+    return (
+      <div className="horizontal">
+        <div>row 1</div>
+        <h1>row</h1>
+        <div>row</div>
+        <div>row</div>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+        <h1>row</h1>
+      </div>
+    )
+  }
+}
+
+export default HorizontalCardList;
