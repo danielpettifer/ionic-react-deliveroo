cac366bb065fe7423013d9b191fd18574864db21
Daniel Pettifer <danielpettifer@me.com>
2020-03-21 23:25:13 +0000

    refactored card to use styled components

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import './Card.css';
+import styled from 'styled-components'
+
+const Cardbase = styled.div `
+    display: flex;
+    min-width: 80px;
+    height: 80px;
+    border-radius: 5px;
+    background: orange;
+    margin-right: 8px;
+    align-items: flex-end;
+    padding: 8px;
+    font-size: 10px;
+    background-image: url(${props => props.img});
+    background-size: contain;
+`
+
+const Title = styled.div `
+    font-size: 12px;
+    line-height: 12px;
+    text-transform: capitalize;
+    color: #fff;
+    font-weight: 600;
+    margin-bottom: 4px;
+`
 
 const Card = props => (
-  <div 
-    className="Card"
-    style={{ backgroundImage: `url('${props.img}')` }}>
-      <h3>{props.title}</h3>
-  </div>
+  <Cardbase>
+      <Title>{props.title}</Title>
+  </Cardbase>
 )
 
 export default Card;
