8115c8118c70c9b4d10431970fdb172a9fc2c452
Daniel Pettifer <danielpettifer@me.com>
2020-03-21 22:36:06 +0000

    added temp cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { IonSegment, IonSegmentButton, IonLabel } from '@ionic/react';
 import './Card.css';
 
 class Card extends React.Component {
--- src/components/HorizontalCardList/HorizontalCardList.js
+++ src/components/HorizontalCardList/HorizontalCardList.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import Card from '../Card/Card';
+
 import './HorizontalCardList.css';
 
 class HorizontalCardList extends React.Component {
 
   render() {
     return (
-      <div className="horizontal">
-   
+      <div className="horizontal ion-padding">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     )
   }
