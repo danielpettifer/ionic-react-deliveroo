96662f546918af07ff7be5d21fbbb7da5356c556
Daniel Pettifer <danielpettifer@me.com>
2020-03-21 23:43:49 +0000

    refactored horizontal card list and fixed broken image path for cards

--- src/components/HorizontalCardList/HorizontalCardList.js
+++ src/components/HorizontalCardList/HorizontalCardList.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import Card from '../Card/Card';
 
-import './HorizontalCardList.css';
+import styled from 'styled-components';
 
+const HorizontalList = styled.div `
+    display: flex;
+    flex-direction: row;
+    overflow-x: auto;
+    padding-right: 32px !important;
+`
 class HorizontalCardList extends React.Component {
 
   render() {
     return (
-      <div className="horizontal ion-padding">
-        <Card title="Noodles" img="https://www.takeaway.com/foodwiki/uploads/sites/11/2019/08/ramen_2-1080x1080.jpg" />
-        <Card title="Burgers" img="https://www.thechunkychef.com/wp-content/uploads/2015/09/Dr-Pepper-BBQ-Burgers-8-500x375.jpg" />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <HorizontalList className="horizontal ion-padding">
+        <Card 
+          title="Noodles"
+          img="https://www.takeaway.com/foodwiki/uploads/sites/11/2019/08/ramen_2-1080x1080.jpg"
+        />
+        <Card
+          title="Burgers"
+          img="https://www.thechunkychef.com/wp-content/uploads/2015/09/Dr-Pepper-BBQ-Burgers-8-500x375.jpg"
+        />
+      </HorizontalList>
     )
   }
 }
