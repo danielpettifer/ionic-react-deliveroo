35fff2f46e153039a2e013d6a51cbaa7548e1609
Daniel Pettifer <danielpettifer@me.com>
2020-03-21 22:26:33 +0000

    removed unused references and added card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IonSegment, IonSegmentButton, IonLabel } from '@ionic/react';
+import './Card.css';
+
+class Card extends React.Component {
+
+  render() {
+    return (
+      <div className="Card">
+   
+      </div>
+    )
+  }
+}
+
+export default Card;
--- src/components/HorizontalCardList/HorizontalCardList.js
+++ src/components/HorizontalCardList/HorizontalCardList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { IonSegment, IonSegmentButton, IonLabel } from '@ionic/react';
 import './HorizontalCardList.css';
 
 class HorizontalCardList extends React.Component {
@@ -7,28 +6,7 @@ class HorizontalCardList extends React.Component {
   render() {
     return (
       <div className="horizontal">
-        <div>row 1</div>
-        <h1>row</h1>
-        <div>row</div>
-        <div>row</div>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
-        <h1>row</h1>
+   
       </div>
     )
   }
