cfc01b63be37958b84d7c136b47ce4103f4f0708
Daniel Pettifer <danielpettifer@me.com>
2020-03-22 19:36:07 +0000

    added my account header

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,32 +1,21 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle } from '@ionic/react';
 import React from 'react';
-import staticdata from '../staticdata.json';
-import DeliverooHeader from '../components/DeliverooHeader/DeliverooHeader';
-import DeliverooSearch from '../components/DeliverooSearch/DeliverooSearch';
-import DeliverooSegmentControl from '../components/DeliverooSegmentControl/DeliverooSegmentControl';
-import HorizontalCardList from '../components/HorizontalCardList/HorizontalCardList';
-import Notice from '../components/Notice/Notice';
-import Card from '../components/Card/Card';
-
-import './Home.css';
+import { styled } from 'styled-components';
 
 const Account = () => {
   return (
     <IonPage>
-      <IonContent>
-        <DeliverooHeader />
-        <DeliverooSegmentControl />
-        <DeliverooSearch />
-        <HorizontalCardList>
-          {staticdata.smallCards.map(card => (
-            <Card title={card.title} image={card.img} />
-          ))}
-        </HorizontalCardList>
-        <Notice
-          title="Contact-free delivery"
-          content="Your rider will knock, place your order at your door and wait nearby to make sure you receive it."
-        />
-      </IonContent>
+        <IonHeader>
+            <IonToolbar>
+                <IonButtons slot="start">
+                <IonBackButton defaultHref="/" />
+                </IonButtons>
+                <IonTitle>My Account</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent color="light">
+           
+        </IonContent>
     </IonPage>
   );
 };
