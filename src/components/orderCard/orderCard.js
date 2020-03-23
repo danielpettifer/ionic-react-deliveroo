2c3f212467d81b7fcb0b48f45bdab02713465f8b
Daniel Pettifer <danielpettifer@me.com>
2020-03-23 12:04:05 +0000

    added order card

---
+++ src/components/orderCard/orderCard.js
@@ -0,0 +1,90 @@
+import React from 'react';
+// import { IonIcon } from '../../../node_modules/@ionic/react';
+// import { chevronRight} from 'ionicons/icon';
+import styled from 'styled-components'
+
+
+const OrderCard = props => (
+  <Cardbase>
+    <Column>
+        <CardImage image={props.image}></CardImage>
+    </Column>
+    <Column>
+        <Status>{props.status}</Status>
+        <Name>{props.name}</Name>
+        <Row>
+            <Total>{props.total}</Total>
+            •
+            <Time>{props.timestamp}</Time>
+        </Row>
+    </Column>
+    <Column>
+        {/* <IonIcon icon={ chevronRight } /> */}
+    </Column>
+      
+  </Cardbase>
+)
+
+const Status = styled.div `
+`
+
+const Name = styled.div `
+`
+
+const Total = styled.div `
+`
+
+const Time = styled.div `
+`
+
+const Column = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 16px;
+`
+
+const Row = styled.div `
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+const CardImage = styled.div `
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: block;
+    background-image: url(${props => props.image});
+    background-size: cover;
+`
+
+const Title = styled.div `
+    font-size: 12px;
+    line-height: 12px;
+    text-transform: capitalize;
+    color: #fff;
+    font-weight: 600;
+    margin-bottom: 4px;
+    z-index: 1;
+`
+
+const Cardbase = styled.div `
+    display: flex;
+    justify-content: space-between;
+    min-width: 80px;
+    height: 80px;
+    border-radius: 5px;
+    background: var(--ion-color-light-shade);
+    margin-right: 8px;
+    align-items: flex-end;
+    padding: 8px;
+    font-size: 10px;
+    background-size: contain;
+    position: relative;
+    overflow: hidden;
+`
+
+export default OrderCard;
--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,4 +1,5 @@
 import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle } from '@ionic/react';
+import OrderCard from '../components/orderCard/orderCard';
 import orderData from '../orderData.json';
 import React from 'react';
 // import { styled } from 'styled-components';
@@ -20,6 +21,16 @@ const Orders = () => {
         </IonHeader>
         <IonContent color="light">
            <h1>orders</h1>
+
+            {orderData.orders.map(card => (
+                <OrderCard 
+                    image={card.image}
+                    status={card.status}
+                    name={card.name}
+                    total={card.total}
+                    timestamp={card.timestamp}
+                />
+            ))}
         </IonContent>
     </IonPage>
   );
