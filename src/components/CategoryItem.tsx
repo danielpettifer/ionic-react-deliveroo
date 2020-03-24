4d5dd164ee30bffff20ba3a0a3532f1b3e94f525
Daniel Pettifer <danielpettifer@me.com>
2020-03-24 13:54:30 +0000

    atyled category cards

--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import {
-  IonItem,
+  IonCard,
   IonLabel,
-  IonAvatar,
 } from '@ionic/react';
 import { Category } from '../data/categories';
-import styled from '../../node_modules/styled-components';
 
 interface CategoryListItemProps {
   category: Category;
@@ -14,48 +12,17 @@ interface CategoryListItemProps {
 const CategoryListItem: React.FC<CategoryListItemProps> = ({ category }) => {
   return (
     <>
-      <IonItem routerLink={`/home/categorydetail/${category.id}`} detail={false} lines="full">
-        <IonAvatar slot="start">
-          <img src={category.image} alt="restaurant" />
-        </IonAvatar>
-        <IonLabel>
+      <IonCard
+        className="category-list-item"
+        routerLink={`/home/categorydetail/${category.id}`}
+      >
+        <img src={category.image} alt="restaurant" />
+        <IonLabel className="label-bottom">
           <h3>{category.name}</h3>
         </IonLabel>
-
-      </IonItem>
-
-      {/* <Cardbase>
-        <Title>{category.name}</Title>
-        <img src={category.image} alt="restaurant" />
-      </Cardbase> */}
+      </IonCard>
     </>
   );
 };
-
-const Title = styled.div`
-    font-size: 12px;
-    line-height: 12px;
-    text-transform: capitalize;
-    color: #fff;
-    font-weight: 600;
-    margin-bottom: 4px;
-    z-index: 1;
-`
-
-const Cardbase = styled.div`
-    display: flex;
-    min-width: 80px;
-    height: 80px;
-    border-radius: 5px;
-    background: var(--ion-color-light-shade);
-    background-image: url({category.name})
-    margin-right: 8px;
-    align-items: flex-end;
-    padding: 8px;
-    font-size: 10px;
-    background-size: contain;
-    position: relative;
-    overflow: hidden;
-    `
 
 export default CategoryListItem;
