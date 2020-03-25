eea9eeb34f3aa3e4d87cbb968866dd18117c77fd
Daniel Pettifer <danielpettifer@me.com>
2020-03-25 10:50:50 +0000

    updated category images

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -7,32 +7,32 @@ export interface Category {
 const categories: Category[] = [
   {
     name: 'Chinese',
-    image: 'https://www.takeaway.com/foodwiki/uploads/sites/11/2019/08/ramen_2-1080x1080.jpg',
+    image: 'https://www.thespruceeats.com/thmb/cVIMAwzKdR_GY6bDvivKPkQUyr8=/2733x2733/smart/filters:no_upscale()/chinese-take-out-472927590-57d31fff3df78c5833464e7b.jpg',
     id: 0
   },
   {
     name: 'Pizza',
-    image: 'https://www.takeaway.com/foodwiki/uploads/sites/11/2019/08/ramen_2-1080x1080.jpg',
+    image: 'https://www.marcos.com/uploads/2018/03/7201_Marcos_AllMeat_RetinaMed_2.jpg',
     id: 1
   },
   {
     name: 'Burgers',
-    image: 'https://www.takeaway.com/foodwiki/uploads/sites/11/2019/08/ramen_2-1080x1080.jpg',
+    image: 'https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-5.jpg',
     id: 2
   },
   {
     name: 'Fried Chicken',
-    image: 'https://www.takeaway.com/foodwiki/uploads/sites/11/2019/08/ramen_2-1080x1080.jpg',
+    image: 'https://sweetandsavorymeals.com/wp-content/uploads/2019/07/Korean-Fried-Chicken-3.jpg',
     id: 3
   },
   {
     name: 'Italian',
-    image: 'https://www.takeaway.com/foodwiki/uploads/sites/11/2019/08/ramen_2-1080x1080.jpg',
+    image: 'https://cdn.shopify.com/s/files/1/0421/0685/products/a7b48ec0-8074-4f85-9a83-d11140d2bb9e_1024x1024.jpg?v=1560350806',
     id: 4
   },
   {
     name: 'Tapas',
-    image: 'https://www.takeaway.com/foodwiki/uploads/sites/11/2019/08/ramen_2-1080x1080.jpg',
+    image: 'https://www.recipetineats.com/wp-content/uploads/2016/08/Easy-Spanish-Tapas.jpg',
     id: 5
   }
 ];
