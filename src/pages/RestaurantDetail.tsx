66192709dfc9f278a6c109444b2c2106e7f3b6d4
Daniel Pettifer <danielpettifer@me.com>
2020-03-25 19:06:17 +0000

    fixed image header

--- src/pages/RestaurantDetail.tsx
+++ src/pages/RestaurantDetail.tsx
@@ -11,7 +11,8 @@ import {
     useIonViewWillEnter
 } from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
-
+import styled from '../../node_modules/styled-components';
+import { arrowBack } from 'ionicons/icons';
 interface ViewRestaurantProps extends RouteComponentProps<{ id: string; }> { }
 
 const RestaurantDetail: React.FC<ViewRestaurantProps> = ({ match }) => {
@@ -26,66 +27,88 @@ const RestaurantDetail: React.FC<ViewRestaurantProps> = ({ match }) => {
     return (
         <IonPage>
             <IonHeader>
-                <IonToolbar>
+                <IonToolbar className="start-opacity-zero">
                     <IonButtons slot="start">
-                        <IonBackButton
-                            defaultHref="/"
-                            text=""
-                        />
-
+                        <IonBackButton defaultHref="/" text="" icon={arrowBack} />
                     </IonButtons>
                     <IonTitle>
-                        {restaurant ? (
-                            <>
-                                {restaurant.name}
-                            </>
-
-                        ) :
-
-                            <>
-                                unkown
-                            </>
-                        }
-
-
+                        {restaurant ? (<>{restaurant.name}</>) : <>Unkown</>}
                     </IonTitle>
                 </IonToolbar>
             </IonHeader>
+
             <IonContent fullscreen>
-                <IonHeader collapse="condense" className="header-large">
-                    <IonToolbar className="bottom">
-                        <IonTitle>
-                            {restaurant ? (<>{restaurant.name}</>) : <></>}
+                <IonHeader collapse="condense" className="header-large" no-border>
+                    <IonToolbar className="bottom" color="transparent" >
+
+                        <IonTitle color="transparent" className="transparent">
+                            {restaurant ? (<>{restaurant.name}</>) : <>Unkown</>}
                         </IonTitle>
                     </IonToolbar>
+
                 </IonHeader>
+                <HeaderImage>
+                    {restaurant ? (
+                        <><img src={restaurant.image} /></>
+                    ) : (<></>)}
+                </HeaderImage>
+                <ContentWrapper>
+                    <ContentHeader>
+                        {restaurant ? (<>{restaurant.name}</>) : <>Unkown</>}
+                    </ContentHeader>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
 
-
-                <h1>rofffw</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1>
-                <h1>row</h1> <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                    <h1>row</h1>
+                </ContentWrapper>
 
 
             </IonContent>
+
         </IonPage >
     );
 };
+
+const ContentHeader = styled.div`
+    font-size:20px;
+    color:#000;
+    font-weight: 800;
+    padding: 16px 16px 32px;
+    background: white;
+    position: relative;
+    top: -34px;
+`
+
+const ContentWrapper = styled.div`
+    z-index: 9999999999;
+    position: relative;
+    background: white;
+`
+
+const HeaderImage = styled.div`
+
+position: fixed;
+top: 0;
+left: 0;
+right: 0;
+min-height: 150px;
+display:block;
+z-index: 0;
+`
 
 export default RestaurantDetail;
